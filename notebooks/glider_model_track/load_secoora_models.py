19450d5d648bcf2fc647330ae0c4212519499913
ocefpaf <ocefpaf@gmail.com>
2014-05-14 10:34:11 -0300

    Starting point to glider model comparison.

---
+++ notebooks/glider_model_track/load_secoora_models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# load_secoora_models.py
+#
+# purpose:  Compare with "load_secoora_models.m"
+# author:   Filipe P. A. Fernandes
+# e-mail:   ocefpaf@gmail
+# web:      http://ocefpaf.github.io/
+# created:  14-May-2014
+# modified: Wed 14 May 2014 10:32:04 AM BRT
+#
+# obs: https://github.com/nctoolbox/nctoolbox/blob/master/demos/contrib/
+#
