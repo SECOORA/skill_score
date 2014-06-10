383b4347fb28e1cd9e936c911ffa9caed31ef12f
ocefpaf <ocefpaf@gmail.com>
2014-06-10 13:03:16 -0300

    Test latest checkout.

--- notebooks/z-coordinate/netcdf_promote.py
+++ notebooks/z-coordinate/netcdf_promote.py
@@ -4,12 +4,13 @@
 # <markdowncell>
 
 # https://github.com/SciTools/iris/pull/1070
+# 
 # https://github.com/SciTools/iris/pull/1159
 
 # <codecell>
 
 import iris
-print(iris.__version__)  # remotes/origin/netcdf_promote 15b2c96
+print(iris.__version__)  # remotes/origin/netcdf_promote 9f6e7ce
 
 # <codecell>
 
@@ -18,15 +19,16 @@ print(iris.FUTURE.netcdf_promote)
 # <codecell>
 
 url = 'http://geoport.whoi.edu/thredds/dodsC/coawst_4/use/fmrc/coawst_4_use_best.ncd'
+constraint = 'water_surface_height_above_reference_datum'
 
-cube = iris.load_cube(url, 'water_surface_height_above_reference_datum')
+cube = iris.load_cube(url, constraint)
 print(cube)
 
 # <codecell>
 
 iris.FUTURE.netcdf_promote = True
 
-cube = iris.load_cube(url, 'water_surface_height_above_reference_datum')
+cube = iris.load_cube(url, constraint)
 print(cube)
 
 # <codecell>
@@ -47,6 +49,7 @@ reference_terms
 
 url = "http://fvcom.smast.umassd.edu:8080/thredds/dodsC/FVCOM/NECOFS/Forecasts/NECOFS_GOM3_FORECAST.nc"
 
-zeta = iris.load_cube(url, 'sea_surface_height_above_geoid')
-print(zeta)
+cube = iris.load_cube(url, 'sea_surface_height_above_geoid')
+
+print(cube)
 
