429e1035e43c5fbd64043d5a746be3582bdfc21a
ocefpaf <ocefpaf@gmail.com>
2014-07-02 18:11:59 -0300

    mplleaflet test.

--- notebooks/HF_radar/HFR_comparison.py
+++ notebooks/HF_radar/HFR_comparison.py
@@ -12,9 +12,14 @@ from datetime import datetime, timedelta
 
 # Scientific stack.
 import iris
-import cartopy.crs as ccrs
+import numpy as np
+import numpy.ma as ma
 import matplotlib.pyplot as plt
 from netCDF4 import Dataset, num2date, date2index
+
+import mplleaflet
+
+import cartopy.crs as ccrs
 from cartopy.io.img_tiles import MapQuestOpenAerial
 
 # Custom IOOS/ASA modules (available at PyPI).
@@ -23,7 +28,6 @@ from cartopy.io.img_tiles import MapQuestOpenAerial
 from utilities import time_near, shrink, rot2d
 
 # <codecell>
-
 
 def get_hfradar(url, time_slice, n=3):
     u = iris.load_cube(radar_url, 'surface_eastward_sea_water_velocity')
@@ -39,11 +43,12 @@ def get_hfradar(url, time_slice, n=3):
 
     time = uslice.coord('time')
     time = time.units.num2date(time.points[0])
+    
+    lon, lat = np.meshgrid(lon, lat)
 
     return dict(lon=lon, lat=lat, u=u, v=v, time=time)
 
 # <codecell>
-
 
 def get_roms(url, time_slice, n=3):
     """FIXME: Re-write this using iris."""
@@ -71,6 +76,9 @@ def get_roms(url, time_slice, n=3):
 
         u, v = u[::n, ::n], v[::n, ::n]
         lon, lat = lon[::n, ::n], lat[::n, ::n]
+        
+        u = ma.masked_invalid(u)
+        v = ma.masked_invalid(v)
         return dict(lon=lon, lat=lat, u=u, v=v, time=time)
 
 # <codecell>
@@ -84,7 +92,7 @@ time_slice = datetime.utcnow() - timedelta(hours=6)
 radar_url = 'http://hfrnet.ucsd.edu/'
 radar_url += 'thredds/dodsC/HFRNet/USEGC/6km/hourly/RTV'
 
-radar = get_hfradar(radar_url, time_slice)
+radar = get_hfradar(radar_url, time_slice, n=6)
 
 # <codecell>
 
@@ -92,7 +100,11 @@ radar = get_hfradar(radar_url, time_slice)
 model_url = 'http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/'
 model_url += 'SABGOM_Forecast_Model_Run_Collection_best.ncd'
 
-model = get_roms(model_url, time_slice)
+model = get_roms(model_url, time_slice, n=6)
+
+# <markdowncell>
+
+# # Cartopy
 
 # <codecell>
 
@@ -128,4 +140,26 @@ date1 = radar['time'].strftime(US_format)
 date2 = model['time'].strftime(US_format)
 _ = ax.set_title('HFR 6km Velocity at %s\nModel Velocity at %s' %
                  (date1, date2))
+
+# <markdowncell>
+
+# # mplleaflet
+
+# <codecell>
+
+fig, ax = plt.subplots()
+kw = dict(scale=20, headwidth=2)
+
+# No masked array support!
+mask = ~radar['u'].mask
+
+Q = ax.quiver(radar['lon'][mask], radar['lat'][mask],
+              radar['u'][mask], radar['v'][mask], color='black', **kw)
+
+mask = ~model['u'].mask
+
+Q = ax.quiver(model['lon'][mask], model['lat'][mask],
+              model['u'][mask], model['v'][mask], color='red', **kw)
+
+mplleaflet.display(fig=ax.figure)
 
